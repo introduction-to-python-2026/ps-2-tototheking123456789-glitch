c852b53b92efddc246bbcd7f8fb3b1099e2d8d53
YotamHibner <tototheking123456789@gmail.com>
2025-11-04 21:16:14 +0200

    Implement max, mean, and std functions in math_utils
    Implemented functions to find the maximum number, mean, and standard deviation of three numbers. Added input prompts for user to enter numbers.

--- math_utils.py
+++ math_utils.py
@@ -1,10 +1,58 @@
 def find_max_number(num1, num2, num3):
-    pass  # Replace 'pass' with code
+  if num1 > num2 and num1 > num3:
+    return(num1)
+  elif num2 > num1 and num2> num3:
+    return(num2)
+  elif num3 > num1 and num3 > num2:
+    return(num3)
+  elif num1 == num2:
+    return(num1)
+  elif num1 == num3:
+    return(num1)
+  return(num2)
+num1 = int(input("enter a number"))
+num2 = int(input("enter another number"))
+num3 = int(input("enter a third number"))
+find_max_number(num1, num2, num3)
+
+
+
 
 def find_mean(num1, num2, num3):
-    pass  # Replace 'pass' with code
+  return((num1 + num2 + num3)/3)
+    
+num1 = int(input("enter a numbner"))
+num2 = int(input("enter another number"))
+num3 = int(input("enter a third number"))
+find_mean(num1, num2, num3)
+
+
+
+
+
+
+
+
+
+
+num1 = int(input("enter a numbner"))
+num2 = int(input("enter another number"))
+num3 = int(input("enter a third number"))
+
+
+def find_mean(num1, num2, num3):
+  return((num1 + num2 + num3)/3)
+    
 
 def find_mean_std(num1, num2, num3):
-    mean = find_mean(num1, num2, num3)
-    pass  # Replace 'pass' with code
+  mean = find_mean(num1, num2, num3)
+  return((((num1-mean)**2 +(num2-mean)**2 + (num3-mean)**2)/3)**0.5) 
+
+
+print(find_mean(num1, num2, num3))
+
+print(find_mean_std(num1, num2, num3))
+
+
+
 
