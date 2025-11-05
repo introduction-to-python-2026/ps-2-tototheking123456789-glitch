9ffa5b27ab18d5d7a00d6fa8e8e1f7f511f6cbed
YotamHibner <tototheking123456789@gmail.com>
2025-11-05 13:08:50 +0200

    Fix return statement in find_mean_std function

--- math_utils.py
+++ math_utils.py
@@ -8,8 +8,8 @@ def find_mean(num1, num2, num3):
 
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
-    return (((num1 - mean) ** 2 + (num2 - mean) ** 2 + (num3 - mean) ** 2) / 3) ** 0.5
-
+    std = (((num1 - mean) ** 2 + (num2 - mean) ** 2 + (num3 - mean) ** 2) / 3) ** 0.5
+    return mean, std
 
 # --- main program ---
 num1 = int(input("Enter a number: "))
