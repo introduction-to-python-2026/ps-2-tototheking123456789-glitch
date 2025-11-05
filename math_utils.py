1c542a589f89a30efbd87a4b4890bc7c2363e361
YotamHibner <tototheking123456789@gmail.com>
2025-11-05 13:52:01 +0200

    Replace variable assignment with print statement

--- math_utils.py
+++ math_utils.py
@@ -23,5 +23,4 @@ def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
     std = (((num1 - mean) ** 2 + (num2 - mean) ** 2 + (num3 - mean) ** 2) / 3) ** 0.5
     return mean, std
-
-mean, std = find_mean_std(num1, num2, num3)
+print(find_mean_std(num1, num2, num3))
