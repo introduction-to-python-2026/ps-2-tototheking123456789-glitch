4e67818bcfcb24bb184c6a427640fc3ca77ef3fe
YotamHibner <tototheking123456789@gmail.com>
2025-11-05 13:19:57 +0200

    Improve input prompts and fix comparison operators

--- math_utils.py
+++ math_utils.py
@@ -1,27 +1,26 @@
-num1 = int(input("enter a number"))
-num2 = int(input("enter a number"))
-num3 = int(input("enter a number"))
-
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+num3 = int(input("Enter a third number: "))
 
 def find_max_number(num1, num2, num3):
-    if num1 => num2 and num1 >= num3:
+    if num1 >= num2 and num1 >= num3:
         return num1
-    elif num2 => num1 and num2 >= num3:
+    elif num2 >= num1 and num2 >= num3:
         return num2
     else:
         return num3
-maxnum = find_max_number(num1, num2, num3)
-    
-
 
 def find_mean(num1, num2, num3):
     return (num1 + num2 + num3) / 3
-
 
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
     std = (((num1 - mean) ** 2 + (num2 - mean) ** 2 + (num3 - mean) ** 2) / 3) ** 0.5
     return mean, std
-return maxnum, mean, std
 
+maxnum = find_max_number(num1, num2, num3)
+mean, std = find_mean_std(num1, num2, num3)
 
+print("The maximum number is:", maxnum)
+print("The mean is:", mean)
+print("The standard deviation is:", std)
