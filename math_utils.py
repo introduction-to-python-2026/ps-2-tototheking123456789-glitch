37824ec29660543d98f0d3a010f3c2639f0000b4
YotamHibner <tototheking123456789@gmail.com>
2025-11-05 12:40:30 +0200

    Refactor find_mean to remove user input
    Removed user input for numbers in find_mean function.

--- math_utils.py
+++ math_utils.py
@@ -21,9 +21,6 @@ find_max_number(num1, num2, num3)
 def find_mean(num1, num2, num3):
   return((num1 + num2 + num3)/3)
     
-num1 = int(input("enter a numbner"))
-num2 = int(input("enter another number"))
-num3 = int(input("enter a third number"))
 find_mean(num1, num2, num3)
 
 
