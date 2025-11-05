aded4abe20a67da26f2d0580b52d5337be4c511a
YotamHibner <tototheking123456789@gmail.com>
2025-11-05 13:17:33 +0200

    Implement user input and statistical calculations
    Added user input for three numbers and calculated max, mean, and standard deviation.

--- math_utils.py
+++ math_utils.py
@@ -1,3 +1,8 @@
+num1 = int(input("enter a number"))
+num2 = int(input("enter a number"))
+num3 = int(input("enter a number"))
+
+
 def find_max_number(num1, num2, num3):
     if num1 => num2 and num1 >= num3:
         return num1
@@ -5,6 +10,8 @@ def find_max_number(num1, num2, num3):
         return num2
     else:
         return num3
+maxnum = find_max_number(num1, num2, num3)
+    
 
 
 def find_mean(num1, num2, num3):
@@ -15,12 +22,6 @@ def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
     std = (((num1 - mean) ** 2 + (num2 - mean) ** 2 + (num3 - mean) ** 2) / 3) ** 0.5
     return mean, std
+return maxnum, mean, std
 
-# --- main program ---
-num1 = int(input("Enter a number: "))
-num2 = int(input("Enter another number: "))
-num3 = int(input("Enter a third number: "))
 
-print("The maximum number is:", find_max_number(num1, num2, num3))
-print("The mean is:", find_mean(num1, num2, num3))
-print("The standard deviation is:", find_mean_std(num1, num2, num3))
