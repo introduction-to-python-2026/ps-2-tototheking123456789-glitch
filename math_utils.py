0384a42d0890e5d18ad99a862b62b7ba9266bde1
YotamHibner <tototheking123456789@gmail.com>
2025-11-05 13:41:21 +0200

    Simplify input handling in math_utils.py
    Removed redundant input prompts for numbers.

--- math_utils.py
+++ math_utils.py
@@ -3,11 +3,6 @@ num2 = int(input("Enter another number: "))
 num3 = int(input("Enter a third number: "))
 
 
-num1 = int(input("Enter a number: "))
-num2 = int(input("Enter another number: "))
-num3 = int(input("Enter a third number: "))
-
-
 def find_max_number(num1, num2, num3):
     if num1 >= num2 and num1 >= num3:
         return num1
