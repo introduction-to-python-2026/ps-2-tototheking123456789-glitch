c1c456802ff8584166fbee86f4e235156c6d1042
YotamHibner <tototheking123456789@gmail.com>
2025-11-05 13:07:00 +0200

    Refactor math utility functions and update main program
    Refactored functions to use built-in max and simplified mean calculation. Updated main program to include user input and print results.

--- math_utils.py
+++ math_utils.py
@@ -1,43 +1,21 @@
 def find_max_number(num1, num2, num3):
-  if num1 > num2 and num1 > num3:
-    return(num1)
-  elif num2 > num1 and num2> num3:
-    return(num2)
-  elif num3 > num1 and num3 > num2:
-    return(num3)
-  elif num1 == num2:
-    return(num1)
-  elif num1 == num3:
-    return(num1)
-  else:  
-    return(num2)
-num1 = int(input("enter a number"))
-num2 = int(input("enter another number"))
-num3 = int(input("enter a third number"))
-find_max_number(num1, num2, num3)
-
-
+    return max(num1, num2, num3)
 
 
 def find_mean(num1, num2, num3):
-  return((num1 + num2 + num3)/3)
-
-
-num1 = int(input("enter a number"))
-num2 = int(input("enter a number"))
-num3 = int(input("enter a number"))
-
-find_mean(num1, num2, num3)
+    return (num1 + num2 + num3) / 3
 
 
 def find_mean_std(num1, num2, num3):
-  mean = find_mean(num1, num2, num3)
-  return((((num1-mean)**2 +(num2-mean)**2 + (num3-mean)**2)/3)**0.5) 
+    mean = find_mean(num1, num2, num3)
+    return (((num1 - mean) ** 2 + (num2 - mean) ** 2 + (num3 - mean) ** 2) / 3) ** 0.5
 
 
-find_mean(num1, num2, num3)
-find_mean_std(num1, num2, num3)
+# --- main program ---
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+num3 = int(input("Enter a third number: "))
 
-
-
-
+print("The maximum number is:", find_max_number(num1, num2, num3))
+print("The mean is:", find_mean(num1, num2, num3))
+print("The standard deviation is:", find_mean_std(num1, num2, num3))
