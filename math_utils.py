6bd617ce643aac30eb99e9af1597197f31a638ab
YotamHibner <tototheking123456789@gmail.com>
2025-11-08 20:37:15 +0200

    Update function calls to return values directly

--- math_utils.py
+++ math_utils.py
@@ -8,15 +8,15 @@ def find_max_number(num1, num2, num3):
           return num3
 
 
-return find_max_number(num1, num2, num3)
+find_max_number(num1, num2, num3)
 
 
 def find_mean(num1, num2, num3):
       mean = (num1 + num2 + num3) / 3
       return mean 
-return find_mean(num1, num2, num3)
+find_mean(num1, num2, num3)
     
 def find_mean_std(num1, num2, num3):
       std = (((num1 - mean) ** 2 + (num2 - mean) ** 2 + (num3 - mean) ** 2) / 3) ** 0.5
       return std
-return find_mean_std(num1, num2, num3)
+find_mean_std(num1, num2, num3)
