bb173a0831b498550c7842c1fb485ffe3c9e18f1
YotamHibner <tototheking123456789@gmail.com>
2025-11-08 20:30:26 +0200

    Replace find_max_number with find_mean function

--- math_utils.py
+++ math_utils.py
@@ -8,7 +8,7 @@ def find_max_number(num1, num2, num3):
           return num3
 
 
-return find_max_number(num1, num2, num3)
+return find_mean(num1, num2, num3)
 def find_mean(num1, num2, num3):
       return (num1 + num2 + num3) / 3
     
