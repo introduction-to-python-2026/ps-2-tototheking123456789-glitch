cdb924e30321e377d54101d4339d7efe4f8c7903
YotamHibner <tototheking123456789@gmail.com>
2025-11-08 19:41:10 +0200

    Fix indentation in find_max_number and find_mean_std

--- math_utils.py
+++ math_utils.py
@@ -1,20 +1,22 @@
 
 def find_max_number(num1, num2, num3):
-    if num1 >= num2 and num1 >= num3:
-        return num1
-    elif num2 >= num1 and num2 >= num3:
-        return num2
-    else:
-        return num3
+      if num1 >= num2 and num1 >= num3:
+          
+          return num1
+      elif num2 >= num1 and num2 >= num3:
+          return num2
+      else:
+          return num3
+
+
 maxnum = find_max_number(num1, num2, num3)
 
 print(maxnum)
-
 def find_mean(num1, num2, num3):
     return (num1 + num2 + num3) / 3
-
+    
 def find_mean_std(num1, num2, num3):
-    mean = find_mean(num1, num2, num3)
-    std = (((num1 - mean) ** 2 + (num2 - mean) ** 2 + (num3 - mean) ** 2) / 3) ** 0.5
-    return mean, std
+     mean = find_mean(num1, num2, num3)
+     std = (((num1 - mean) ** 2 + (num2 - mean) ** 2 + (num3 - mean) ** 2) / 3) ** 0.5
+     return mean, std
 print(find_mean_std(num1, num2, num3))
