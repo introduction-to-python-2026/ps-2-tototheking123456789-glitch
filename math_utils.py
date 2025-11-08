d8c5e12d86473ecf6efec65b3b9cc9e6e4644ca7
YotamHibner <tototheking123456789@gmail.com>
2025-11-08 20:34:24 +0200

    Replace mean calculation with max number function

--- math_utils.py
+++ math_utils.py
@@ -8,13 +8,14 @@ def find_max_number(num1, num2, num3):
           return num3
 
 
-return find_mean(num1, num2, num3)
+return find_max_number(num1, num2, num3)
+
+
 def find_mean(num1, num2, num3):
-      return (num1 + num2 + num3) / 3
+      mean = (num1 + num2 + num3) / 3
+      return mean 
     
 def find_mean_std(num1, num2, num3):
-      mean = find_mean(num1, num2, num3)
       std = (((num1 - mean) ** 2 + (num2 - mean) ** 2 + (num3 - mean) ** 2) / 3) ** 0.5
-      return mean
       return std
 return find_mean_std(num1, num2, num3)
