1237daf9a1af28c6d0d467e7a5f2798a8ce1d2e6
YotamHibner <tototheking123456789@gmail.com>
2025-11-08 20:35:57 +0200

    Correct return statement in find_mean function
    Fix return statement in find_mean function.

--- math_utils.py
+++ math_utils.py
@@ -14,6 +14,7 @@ return find_max_number(num1, num2, num3)
 def find_mean(num1, num2, num3):
       mean = (num1 + num2 + num3) / 3
       return mean 
+return find_mean(num1, num2, num3)
     
 def find_mean_std(num1, num2, num3):
       std = (((num1 - mean) ** 2 + (num2 - mean) ** 2 + (num3 - mean) ** 2) / 3) ** 0.5
